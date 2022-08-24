dd372c403419e771623307f7ea4cec7554c1ab55
Kira Corbett <kiracorbett00@gmail.com>
2022-08-24 10:47:34 -0700

    init Navbar

---
+++ project-site/src/Navbar.js
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+const Navbar = () => {
+  return ( 
+    <nav className="navbar">
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/">Experience</a>
+        <a href="/">Software</a>
+        <a href="/">Athletics</a>
+      </div>
+    </nav>
+   );
+}
+ 
+export default Navbar;
