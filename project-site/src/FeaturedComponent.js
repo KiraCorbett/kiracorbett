592377d5856dc8c65eed143757c630bc0750b7ce
Kira Corbett <kiracorbett00@gmail.com>
2022-11-01 05:32:59 -0700

    wip: reformatting homepage styles

--- project-site/src/FeaturedComponent.js
+++ project-site/src/FeaturedComponent.js
@@ -3,12 +3,8 @@ import './FeaturedComponent.css';
 
 const FeaturedComponent = () => {
   return ( 
-    <div className="featured">
-      <div className="projects">
-        <div className="gallery">
-          <img src={crow} alt="crow logo" />
-        </div>
-      </div>
+    <div className="gallery">
+      <img src={crow} alt="crow logo" />
     </div>
    );
 }
--- project-site/src/Home.js
+++ project-site/src/Home.js
@@ -1,21 +1,25 @@
-import Figure from 'react-bootstrap/Figure';
+import bike from './images/gbw-bike.jpg';
 import FeaturedComponent from './FeaturedComponent';
 
 import './Home.css'
-import crow from './images/crow.png';
 
 const Home = () => {
   return (
     <div className="home">
+    <div className="landing">
+      <img src={bike} alt="bike"/>
+    </div>
       <div className="elements">
         <h1>Software Developer</h1>
         <h1>HCI Researcher</h1>
         <h1>Athlete</h1>
       </div>
-      <h1>Featured Projects</h1>
+      <div className="featured">
+        <h1>Featured Projects</h1>
         <FeaturedComponent />
         <FeaturedComponent />
         <FeaturedComponent />
+      </div>
       <div className="skills">
         <h3>Dev</h3>
         <h3>Design</h3>
