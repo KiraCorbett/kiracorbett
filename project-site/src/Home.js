29d28f13eeb9f77f8f2c6b39321addf640bebaf3
Kira Corbett <kiracorbett00@gmail.com>
2022-12-07 17:02:51 -0800

    Design block

--- project-site/src/Home.js
+++ project-site/src/Home.js
@@ -1,5 +1,11 @@
 import rhizome from './images/rhizome.jpg';
 import crow from './images/crow.png';
+import gravel from './images/gravel.jpg';
+import bob from './images/best-of-both.jpg';
+import bsocial from './images/bob-social.png';
+import cbd from './images/cool-cream.jpg';
+import mint from './images/mint-tincture.jpg';
+import tropical from './images/tropical-tincture.jpg';
 import fractals from './images/fractals.jpg';
 import linkedin from './images/linkedin.png';
 import github from './images/Octocat.png';
@@ -7,6 +13,7 @@ import github from './images/Octocat.png';
 import './Home.css'
 import LandingImage from './components/LandingImage';
 import Panel from './components/Panel';
+import FeaturedComponent from './components/FeaturedComponent';
 
 import React from 'react';
 
@@ -23,8 +30,33 @@ const Home = () => {
       <div className="projects">
         <h1>Featured Projects</h1>
           <Panel heading={"Synthesis: An Autodesk Technology"} video={"https://www.youtube.com/embed/MEW_JCmHTo0"} description={"Built by students, for students, Synthesis is an open source simulator used to design, test, and experiment with 3D CAD models."} link={"http://synthesis.autodesk.com"} btnTitle={"Synthesis Website"} skills={"C# | Python | Unity | UX | Project Management"} />
-
           <Panel heading={"Costa Rica: Nature & Cultural Immersion"} video={"https://www.google.com/maps/embed?pb=!4v1577667882368!6m8!1m7!1sCAoSLEFGMVFpcFBoeDl4QmR6NUpxcHQteUdSR2VHS3YweVJnRnJsZFhmZ0FCd2VC!2m2!1d8.540835399999999!2d-83.57096399999999!3f17.629331880410597!4f-9.639552171459115!5f0.7820865974627469"} description={"As a computer science major, I wanted a way to provide a diverse perspective to my current skills and thinking. To accomplish this, I decided to study in Costa Rica to capture the experiences on family farms and ecosystems. This project documents our geographical class explorations and through immersive 360 photo spheres."} link={"https://kiracorbett.github.io/costa-rica/"} btnTitle={"Costa Rica Mapping"} skills={"Adobe Premiere Pro | Photoshop | CAD | Content Creation"} />
+        </div>
+
+        <div>
+          <div>
+            <LandingImage image={fractals} mid={"Design"} />
+            <h1>Product Design</h1>
+            <FeaturedComponent image={mint} />
+            <FeaturedComponent image={cbd} />
+            <FeaturedComponent image={tropical} />
+          </div>
+
+          {/* <div className="crow">
+            <h1>Graphic Design</h1>
+            <img src={crow} className="crow"></img>
+            <img src={crow} className="crow"></img>
+            <img src={crow} className="crow"></img>
+          </div> */}
+
+          <div className="design">
+            <h1>Content Creation</h1>
+            <img src={bsocial}></img>
+          </div>
+
+          {/* <FeaturedComponent image={gravel} title="Dirty Freehub Gravel Cycling" subtitle="Technologies Engineer and Content Creator" skills={"Website | Video Editing | Podcasting"}/>
+          <FeaturedComponent image={bob} title="Best of Both Bike Race" subtitle="Content Creator & Race Planning" skills={"Video Editing | Social Media"}/>
+          <FeaturedComponent image={crow} title="Tree and Crow & Yabbaganics" subtitle="Founder | Business | Project Management" skills={"Entrepreneurship"}/> */}
         </div>
 
         <div>
