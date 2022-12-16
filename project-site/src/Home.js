e4e29582d3f8a93f399e5aa09d5333952980e134
Kira Corbett <kiracorbett00@gmail.com>
2022-12-15 21:58:16 -0800

    Reorganized

--- project-site/src/Home.js
+++ project-site/src/Home.js
@@ -2,6 +2,8 @@ import rhizome from './images/rhizome.jpg';
 import crow from './images/crow.png';
 import gravel from './images/gravel.jpg';
 import bob from './images/best-of-both.jpg';
+import gbw from './images/gbw-bike.jpg';
+import wellsaver from './images/wellsaver.png'
 import bsocial from './images/bob-social.png';
 import cbd from './images/cool-cream.jpg';
 import mint from './images/mint-tincture.jpg';
@@ -28,8 +30,9 @@ const Home = () => {
       </div>
 
       <div className="projects">
-        <h1>Featured Projects</h1>
+        <h1>Featured Development Projects</h1>
           <Panel heading={"Synthesis: An Autodesk Technology"} video={"https://www.youtube.com/embed/MEW_JCmHTo0"} description={"Built by students, for students, Synthesis is an open source simulator used to design, test, and experiment with 3D CAD models."} link={"http://synthesis.autodesk.com"} btnTitle={"Synthesis Website"} skills={"C# | Python | Unity | UX | Project Management"} />
+          <Panel heading={"WellSaver"} image={wellsaver} description={"WellSaver is an emergency alert system for skiers/snowboarders stuck in tree wells."} skills={"C++ | JavaScript | Grasshopper | Python"}/>
           <Panel heading={"Costa Rica: Nature & Cultural Immersion"} video={"https://www.google.com/maps/embed?pb=!4v1577667882368!6m8!1m7!1sCAoSLEFGMVFpcFBoeDl4QmR6NUpxcHQteUdSR2VHS3YweVJnRnJsZFhmZ0FCd2VC!2m2!1d8.540835399999999!2d-83.57096399999999!3f17.629331880410597!4f-9.639552171459115!5f0.7820865974627469"} description={"As a computer science major, I wanted a way to provide a diverse perspective to my current skills and thinking. To accomplish this, I decided to study in Costa Rica to capture the experiences on family farms and ecosystems. This project documents our geographical class explorations and through immersive 360 photo spheres."} link={"https://kiracorbett.github.io/costa-rica/"} btnTitle={"Costa Rica Mapping"} skills={"Adobe Premiere Pro | Photoshop | CAD | Content Creation"} />
         </div>
 
@@ -60,9 +63,15 @@ const Home = () => {
         </div>
 
         <div>
-          <LandingImage image={fractals} mid={"HCI Researcher"}/>
+          <LandingImage image={fractals} mid={"HCI Research"}/>
 
           <Panel heading={"Rhizome: Towards and Information Centric OS Shell"} image={rhizome} description={"Rhizome is an information-centric model that uses different interaction methods than traditional desktop systems to measure the cognitive load each OS shell has on a its users. The results indicate that people were 60% more efficient on average in completing their task using Rhizome instead of a traditional WIMP model."} link={"https://ir.library.oregonstate.edu/concern/honors_college_theses/3t945z87r"} btnTitle={"Oregon State University Thesis"} skills={"Flutter SDK | Dart | User Research | Human-Computer Interaction"} />
+        </div>
+
+        <div className="contact">
+          <LandingImage image={fractals} mid={"Kira Corbett"}/>
+          <p>Email: kiracorbett00@gmail.com</p>
+          <p>GitHub: @KiraCorbett</p>
         </div>
 
       {/* <div className="skills">
