64d0a42fd6658e7d085b575dfa8014873a988797
Kira Corbett <kiracorbett00@gmail.com>
2022-12-20 11:26:19 -0800

    Updates

---
+++ precache-manifest.5e6c66abf095fb85a42f0cd38f299762.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "867d353cfd1f7efe12ea",
+    "url": "/static/css/main.4ccb00f1.chunk.css"
+  },
+  {
+    "revision": "867d353cfd1f7efe12ea",
+    "url": "/static/js/main.867d353c.chunk.js"
+  },
+  {
+    "revision": "d2fd5e860431f1be0b24",
+    "url": "/static/js/1.d2fd5e86.chunk.js"
+  },
+  {
+    "revision": "061d15d6ccccac69b135",
+    "url": "/static/css/2.72d39ecf.chunk.css"
+  },
+  {
+    "revision": "061d15d6ccccac69b135",
+    "url": "/static/js/2.061d15d6.chunk.js"
+  },
+  {
+    "revision": "fab0941ce2ce7dc200d8",
+    "url": "/static/js/runtime~main.fab0941c.js"
+  },
+  {
+    "revision": "a51d12cc505580836f4eccb71714aae9",
+    "url": "/static/media/fractals.a51d12cc.jpg"
+  },
+  {
+    "revision": "3485c003dda9c19e4b50ab274d39cda3",
+    "url": "/static/media/rhizome.3485c003.jpg"
+  },
+  {
+    "revision": "040b1c0170a51bef8fd90e9daa6f3ded",
+    "url": "/static/media/wellsaver.040b1c01.png"
+  },
+  {
+    "revision": "e1f0729fd8a114067438a40427842f8b",
+    "url": "/static/media/cool-cream.e1f0729f.jpg"
+  },
+  {
+    "revision": "188291a747a400686b8b8e9e3ca33e7f",
+    "url": "/static/media/mint-tincture.188291a7.jpg"
+  },
+  {
+    "revision": "3ee996516986641bba906d00a82af69f",
+    "url": "/static/media/tropical-tincture.3ee99651.jpg"
+  },
+  {
+    "revision": "6f9a03bd4554e5454de1c79f4c91aadf",
+    "url": "/static/media/linkedin.6f9a03bd.png"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "3fe868a1a9930b59d94d2c1d79461e3c",
+    "url": "/static/media/Montserrat-Regular.3fe868a1.ttf"
+  },
+  {
+    "revision": "c5a465bd16b9cdb3476627e450fc51fb",
+    "url": "/static/media/gravel.c5a465bd.jpg"
+  },
+  {
+    "revision": "8051060a1f6e8fd6125f5d9e9513a267",
+    "url": "/static/media/best-of-both.8051060a.jpg"
+  },
+  {
+    "revision": "15a27001da43ea2f766cca7f49312e11",
+    "url": "/static/media/gbw-bike.15a27001.jpg"
+  },
+  {
+    "revision": "b1b4795ae0606cedb022028ee7188aa7",
+    "url": "/static/media/content.b1b4795a.png"
+  },
+  {
+    "revision": "b57b89fb89d37603b652d44ad3f32a13",
+    "url": "/static/media/team-up.b57b89fb.png"
+  },
+  {
+    "revision": "e4d7682e91e6f32497a7dcd0c5b129e0",
+    "url": "/static/media/dark.e4d7682e.png"
+  },
+  {
+    "revision": "06d90bed3c9a1ed46662741ec130568a",
+    "url": "/static/media/bob-social.06d90bed.png"
+  },
+  {
+    "revision": "881ac5e515c0a489b13479253c1f21ad",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/kiracorbett/precache-manifest.983b0395b23686589fc91dc4dd006921.js"
+  "/precache-manifest.5e6c66abf095fb85a42f0cd38f299762.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/kiracorbett/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
