e7649dd6b3364ae243146b50be4c4e16f2c3e8c6
Kira Corbett <kiracorbett00@gmail.com>
2023-09-26 20:42:41 -0700

    old site

--- src/Home.js
+++ src/Home.js
@@ -26,7 +26,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="home">
-      <LandingImage image={fractals} left={"Design"} mid={"HCI Researcher"} right={"Developer"}/>
+      <LandingImage image={fractals} mid={"Kira Corbett"} />
 
       <div className="socials">
         <a href="https://www.linkedin.com/in/kira-corbett/" target="_blank"><img src={linkedin} /></a>
@@ -35,48 +35,42 @@ const Home = () => {
 
       <div className="projects">
         <h1>Featured Development Projects</h1>
-          <Panel heading={"Synthesis: An Autodesk Technology"} video={"https://www.youtube.com/embed/MEW_JCmHTo0"} description={"Built by students, for students, Synthesis is an open source simulator used to design, test, and experiment with 3D CAD models."} link={"http://synthesis.autodesk.com"} btnTitle={"Synthesis Website"} skills={"C# | Python | Unity | UX | Project Management"} />
-          <Panel heading={"WellSaver"} image={wellsaver} description={"WellSaver is an emergency alert system for skiers/snowboarders stuck in tree wells."} skills={"C++ | JavaScript | Grasshopper | Python"}/>
-          <Panel heading={"Costa Rica: Nature & Cultural Immersion"} video={"https://www.google.com/maps/embed?pb=!4v1577667882368!6m8!1m7!1sCAoSLEFGMVFpcFBoeDl4QmR6NUpxcHQteUdSR2VHS3YweVJnRnJsZFhmZ0FCd2VC!2m2!1d8.540835399999999!2d-83.57096399999999!3f17.629331880410597!4f-9.639552171459115!5f0.7820865974627469"} description={"As a computer science major, I wanted a way to provide a diverse perspective to my current skills and thinking. To accomplish this, I decided to study in Costa Rica to capture the experiences on family farms and ecosystems. This project documents our geographical class explorations and through immersive 360 photo spheres."} link={"https://kiracorbett.github.io/costa-rica/"} btnTitle={"Costa Rica Mapping"} skills={"Adobe Premiere Pro | Photoshop | CAD | Content Creation"} />
+        <Panel heading={"Synthesis: An Autodesk Technology"} video={"https://www.youtube.com/embed/MEW_JCmHTo0"} description={"Built by students, for students, Synthesis is an open source simulator used to design, test, and experiment with 3D CAD models."} link={"http://synthesis.autodesk.com"} btnTitle={"Synthesis Website"} skills={"C# | Python | Unity | UX | Project Management"} />
+        <Panel heading={"WellSaver"} image={wellsaver} description={"WellSaver is an emergency alert system for skiers/snowboarders stuck in tree wells."} skills={"C++ | JavaScript | Grasshopper | Python"} />
+        <Panel heading={"Costa Rica: Nature & Cultural Immersion"} video={"https://www.google.com/maps/embed?pb=!4v1577667882368!6m8!1m7!1sCAoSLEFGMVFpcFBoeDl4QmR6NUpxcHQteUdSR2VHS3YweVJnRnJsZFhmZ0FCd2VC!2m2!1d8.540835399999999!2d-83.57096399999999!3f17.629331880410597!4f-9.639552171459115!5f0.7820865974627469"} description={"As a computer science major, I wanted a way to provide a diverse perspective to my current skills and thinking. To accomplish this, I decided to study in Costa Rica to capture the experiences on family farms and ecosystems. This project documents our geographical class explorations and through immersive 360 photo spheres."} link={"https://kiracorbett.github.io/costa-rica/"} btnTitle={"Costa Rica Mapping"} skills={"Adobe Premiere Pro | Photoshop | CAD | Content Creation"} />
+      </div>
+
+      <div>
+        <div>
+          <LandingTitle image={fractals} mid={"Design"} />
+          {/* <h1>Product Design</h1> */}
+          <FeaturedComponent image={mint} />
+          <FeaturedComponent image={cbd} />
+          <FeaturedComponent image={tropical} />
         </div>
 
-        <div>
-          <div>
-            <LandingTitle image={fractals} mid={"Design"} />
-            {/* <h1>Product Design</h1> */}
-            <FeaturedComponent image={mint} />
-            <FeaturedComponent image={cbd} />
-            <FeaturedComponent image={tropical} />
-          </div>
-
-          {/* <div className="crow">
+        {/* <div className="crow">
             <h1>Graphic Design</h1>
             <img src={crow} className="crow"></img>
             <img src={crow} className="crow"></img>
             <img src={crow} className="crow"></img>
           </div> */}
 
-          {/* <div className="design">
+        {/* <div className="design">
             <h1>Content Creation</h1>
             <img src={content}></img>
           </div> */}
 
-          {/* <FeaturedComponent image={gravel} title="Dirty Freehub Gravel Cycling" subtitle="Technologies Engineer and Content Creator" skills={"Website | Video Editing | Podcasting"}/>
+        {/* <FeaturedComponent image={gravel} title="Dirty Freehub Gravel Cycling" subtitle="Technologies Engineer and Content Creator" skills={"Website | Video Editing | Podcasting"}/>
           <FeaturedComponent image={bob} title="Best of Both Bike Race" subtitle="Content Creator & Race Planning" skills={"Video Editing | Social Media"}/>
           <FeaturedComponent image={crow} title="Tree and Crow & Yabbaganics" subtitle="Founder | Business | Project Management" skills={"Entrepreneurship"}/> */}
-        </div>
+      </div>
 
-        <div>
-          <LandingTitle image={fractals} mid={"HCI Research"}/>
-
-          <Panel heading={"Rhizome: Towards and Information Centric OS Shell"} image={rhizome} description={"Rhizome is an information-centric model that uses different interaction methods than traditional desktop systems to measure the cognitive load each OS shell has on a its users. The results indicate that people were 60% more efficient on average in completing their task using Rhizome instead of a traditional WIMP model."} link={"https://ir.library.oregonstate.edu/concern/honors_college_theses/3t945z87r"} btnTitle={"Oregon State University Thesis"} skills={"Flutter SDK | Dart | User Research | Human-Computer Interaction"} />
-        </div>
-
-        <div className="contact">
-          <LandingTitle image={fractals} mid={"Kira Corbett"}/>
-          <p>Email: kiracorbett00@gmail.com</p>
-          <p>GitHub: @KiraCorbett</p>
-        </div>
+      <div className="contact">
+        <LandingTitle image={fractals} mid={"Kira Corbett"} />
+        <p>Email: kiracorbett00@gmail.com</p>
+        <p>GitHub: @KiraCorbett</p>
+      </div>
 
       {/* <div className="skills">
         <h3>Dev</h3>
