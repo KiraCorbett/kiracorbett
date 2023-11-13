94bb29c1de7106782b0cec7c87c59d77ca37d873
Kira Corbett <kiracorbett00@gmail.com>
2023-11-13 12:16:48 -0800

    Updates

---
+++ precache-manifest.3f4c97fc37e816f635592f7127aaaa22.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f760587077c939c3467",
+    "url": "/static/css/main.6ac8064d.chunk.css"
+  },
+  {
+    "revision": "6f760587077c939c3467",
+    "url": "/static/js/main.6f760587.chunk.js"
+  },
+  {
+    "revision": "8a5b0b70b12187e5da28",
+    "url": "/static/js/1.8a5b0b70.chunk.js"
+  },
+  {
+    "revision": "3356348d3cffb11de3c5",
+    "url": "/static/css/2.457d7fba.chunk.css"
+  },
+  {
+    "revision": "3356348d3cffb11de3c5",
+    "url": "/static/js/2.3356348d.chunk.js"
+  },
+  {
+    "revision": "a46ef3f558368343807d",
+    "url": "/static/js/runtime~main.a46ef3f5.js"
+  },
+  {
+    "revision": "ece2995533493bdfe950264f8567e303",
+    "url": "/static/media/flying.ece29955.jpeg"
+  },
+  {
+    "revision": "3fe868a1a9930b59d94d2c1d79461e3c",
+    "url": "/static/media/Montserrat-Regular.3fe868a1.ttf"
+  },
+  {
+    "revision": "3485c003dda9c19e4b50ab274d39cda3",
+    "url": "/static/media/rhizome.3485c003.jpg"
+  },
+  {
+    "revision": "c5a465bd16b9cdb3476627e450fc51fb",
+    "url": "/static/media/gravel.c5a465bd.jpg"
+  },
+  {
+    "revision": "8051060a1f6e8fd6125f5d9e9513a267",
+    "url": "/static/media/best-of-both.8051060a.jpg"
+  },
+  {
+    "revision": "15a27001da43ea2f766cca7f49312e11",
+    "url": "/static/media/gbw-bike.15a27001.jpg"
+  },
+  {
+    "revision": "b1b4795ae0606cedb022028ee7188aa7",
+    "url": "/static/media/content.b1b4795a.png"
+  },
+  {
+    "revision": "b57b89fb89d37603b652d44ad3f32a13",
+    "url": "/static/media/team-up.b57b89fb.png"
+  },
+  {
+    "revision": "e4d7682e91e6f32497a7dcd0c5b129e0",
+    "url": "/static/media/dark.e4d7682e.png"
+  },
+  {
+    "revision": "040b1c0170a51bef8fd90e9daa6f3ded",
+    "url": "/static/media/wellsaver.040b1c01.png"
+  },
+  {
+    "revision": "06d90bed3c9a1ed46662741ec130568a",
+    "url": "/static/media/bob-social.06d90bed.png"
+  },
+  {
+    "revision": "e1f0729fd8a114067438a40427842f8b",
+    "url": "/static/media/cool-cream.e1f0729f.jpg"
+  },
+  {
+    "revision": "188291a747a400686b8b8e9e3ca33e7f",
+    "url": "/static/media/mint-tincture.188291a7.jpg"
+  },
+  {
+    "revision": "3ee996516986641bba906d00a82af69f",
+    "url": "/static/media/tropical-tincture.3ee99651.jpg"
+  },
+  {
+    "revision": "a51d12cc505580836f4eccb71714aae9",
+    "url": "/static/media/fractals.a51d12cc.jpg"
+  },
+  {
+    "revision": "2d20dd8ee8d9648dcec71ffb0acd0d2f",
+    "url": "/static/media/background-abstract2.2d20dd8e.png"
+  },
+  {
+    "revision": "6f9a03bd4554e5454de1c79f4c91aadf",
+    "url": "/static/media/linkedin.6f9a03bd.png"
+  },
+  {
+    "revision": "ef468e275a2c13d0bc7156549261fde6",
+    "url": "/static/media/Octocat.ef468e27.png"
+  },
+  {
+    "revision": "595942a8ed287fb3501062164a977fe2",
+    "url": "/static/media/logo.595942a8.png"
+  },
+  {
+    "revision": "fdd23867b0cb0ecc8041d087720dd0f7",
+    "url": "/index.html"
+  }
+];
